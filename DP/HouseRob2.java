ed6a8e4a2ff7e4808639276b4236fea89151fa34
Abhijit <abhijitmandal.btech21@sushantuniversity.edu.in>
2024-01-28 14:30:24 +0530

    ADDED Optimized-1 HouseRob2

--- DP/HouseRob2.java
+++ DP/HouseRob2.java
@@ -5,36 +5,35 @@ import java.util.Arrays;
 public class HouseRob2 {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 1};
-
-        int ans = getMaxRobcylic(array);
-        System.out.println(ans);
+//        create 2 arrays here for 2 cycle cases
+        int[] skipLastHouse = new int[array.length - 1];
+        int[] skipFirstHouse = new int[array.length - 1];
+//        copying the elements from the main array here
+        for (int i = 0; i < array.length - 1; i++) {
+            skipLastHouse[i] = array[i];
+            skipFirstHouse[i] = array[i + 1];
+        }
+//        get the loot from both the case
+        int lootSkippingLast = robHelper(skipLastHouse);
+        int lootSkippingFirst = robHelper(skipFirstHouse);
+        int maxLoot = Math.max(lootSkippingLast, lootSkippingFirst);
+        System.out.println(maxLoot);
     }
 
-    private static int getMaxRobcylic(int[] array) {
-        int[] dp1 = new int[array.length];
-        int[] dp2 = new int[array.length];
-        if (array.length == 1) {
+    private static int robHelper(int[] array) {
+        if (array.length <= 1) {
             return array[0];
         }
         if (array.length == 2) {
             return Math.max(array[0], array[1]);
         }
-//        case-1
-        dp1[0] = array[0];
-        dp1[1] = Math.max(array[0], array[1]);
-        for (int i = 2; i <= array.length - 2; i++) {
-            dp1[i] = Math.max(dp1[i - 2] + array[i], dp1[i - 1]);
+        int[] dp = new int[array.length];
+        dp[0] = array[0];
+        dp[1] = Math.max(array[0], array[1]);
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + array[i], dp[i - 1]);
         }
-        System.out.println(Arrays.toString(dp1));
-        int skipLastHouse = dp1[dp1.length - 2];
-//        case-2
-        dp2[1] = array[1];
-        dp2[2] = Math.max(array[1], array[2]);
-        for (int i = 3; i <= array.length - 1; i++) {
-            dp2[i] = Math.max(dp2[i - 2] + array[i], dp2[i - 1]);
-        }
-        System.out.println(Arrays.toString(dp2));
-        int includeLastHouse = dp2[dp2.length - 1];
-        return Math.max(skipLastHouse, includeLastHouse);
+        System.out.println(Arrays.toString(dp));
+        return dp[dp.length - 1];
     }
 }
