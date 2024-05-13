dfb7b759b12d26ae200ac75d94be2cc6870cf85b
Abhijit <abhiiiman2003@gmail.com>
2024-05-13 10:40:05 +0530

    Added Individual Word Reverse Pattern

--- UpGrad_CP/reverseWords.java
+++ UpGrad_CP/reverseWords.java
@@ -10,9 +10,23 @@ public class reverseWords {
         return result.toString().trim();
     }
 
+    private static String revWords(String s) {
+        String[] words = s.split("\\s+");
+        StringBuilder rev = new StringBuilder();
+        for (String word : words) {
+            StringBuilder revWord = new StringBuilder();
+            for (int i = word.length() - 1; i >= 0; i--) {
+                revWord.append(word.charAt(i)); // add from the last to get reversed.
+            }
+            rev.append(revWord).append(" ");
+        }
+        return rev.toString();
+    }
+
     public static void main(String[] args) {
         String str = "Abhijit is a very good boy.";
         String answer = reverse(str);
         System.out.println(answer);
+        System.out.println(revWords(str));
     }
 }
